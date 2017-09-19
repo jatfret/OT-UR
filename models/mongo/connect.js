1f9197bed2fb8d2966290b9a43a1d09b0ce624a2
jatfret <jatfret@gmail.com>
2017-09-19 19:16:13 +0800

    server modify

--- models/mongo/connect.js
+++ models/mongo/connect.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect("mongodb://xxxxxx:27017/papers");
+const db = mongoose.connect("mongodb://papers:fanluo215@localhost:27017/papers");
